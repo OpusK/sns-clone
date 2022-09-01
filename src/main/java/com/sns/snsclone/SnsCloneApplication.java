ea5ab8740546edeb2fb8fe1957a1bae79e4b348f
Kei <opusk44@gmail.com>
2022-09-01 21:00:12 +0900

    exclude DataSourceAutoConfiguration.class for url dummy

--- src/main/java/com/sns/snsclone/SnsCloneApplication.java
+++ src/main/java/com/sns/snsclone/SnsCloneApplication.java
@@ -2,8 +2,9 @@ package com.sns.snsclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SnsCloneApplication {
 
 	public static void main(String[] args) {
