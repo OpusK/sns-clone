f21d35097089ec04cb6100377b916efff2785817
Kei <opusk44@gmail.com>
2022-09-17 21:29:27 +0900

    Add UserController & add empty join method

---
+++ src/main/java/com/sns/snsclone/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.sns.snsclone.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+ 
+    // TODO: implement
+    @PostMapping
+    public void join() {
+
+    }
+}
