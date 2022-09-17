5accec7027acb39e0b8f7956315ab1e72d17b895
Kei <opusk44@gmail.com>
2022-09-17 21:28:39 +0900

    apply object mapped content using UserJoinRequest

--- src/test/java/com/sns/snsclone/controller/UserControllerTest.java
+++ src/test/java/com/sns/snsclone/controller/UserControllerTest.java
@@ -7,6 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sns.snsclone.controller.request.UserJoinRequest;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,17 +22,26 @@ public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
     public void SignUp() {
         String userName = "userName";
         String password = "password";
 
-        mockMvc.perform(post("/api/v1/users/join")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        // TODO: add request body
-                        .content()
-                    ).andDo(print())
-                    .andExpect(status().isOk());
+        // TODO: Mocking
+
+        try {
+            mockMvc.perform(post("/api/v1/users/join")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password)))
+                        ).andDo(print())
+                        .andExpect(status().isOk());
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     @Test
@@ -37,11 +49,17 @@ public class UserControllerTest {
         String userName = "userName";
         String password = "password";
 
-        mockMvc.perform(post("/api/v1/users/join")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        // TODO: add request body
-                        .content()
-                    ).andDo(print())
-                    .andExpect(status().isConflict());
-    } 
+        // TODO: Mocking
+
+        try {
+            mockMvc.perform(post("/api/v1/users/join")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password)))
+                        ).andDo(print())
+                        .andExpect(status().isConflict());
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
