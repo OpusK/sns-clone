143ec408df7feeaef3d1ebff5a87cd419bed880e
Kei <opusk44@gmail.com>
2022-09-17 21:32:06 +0900

    Add UserService (w/ Join method)

--- src/main/java/com/sns/snsclone/controller/UserController.java
+++ src/main/java/com/sns/snsclone/controller/UserController.java
@@ -4,13 +4,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sns.snsclone.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
+ 
+    private final UserService userService;
  
     // TODO: implement
     @PostMapping
     public void join() {
-
+        userService.join();
     }
 }
---
+++ src/main/java/com/sns/snsclone/service/UserService.java
@@ -0,0 +1,12 @@
+package com.sns.snsclone.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    // TODO: implement
+    public void join() {
+
+    }
+}
