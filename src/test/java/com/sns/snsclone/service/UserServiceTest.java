9518b7469c90e5dd7e01fe1326f4ec82c2b4aba0
Kei <opusk44@gmail.com>
2022-09-17 22:43:53 +0900

    update/add SignUp tests for UserService

--- src/test/java/com/sns/snsclone/service/UserServiceTest.java
+++ src/test/java/com/sns/snsclone/service/UserServiceTest.java
@@ -1,9 +1,20 @@
 package com.sns.snsclone.service;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.sns.snsclone.exception.SnsApplicationException;
+import com.sns.snsclone.model.entity.UserEntity;
+import com.sns.snsclone.repository.UserEntityRepository;
 
 @SpringBootTest
 public class UserServiceTest {
@@ -11,11 +22,28 @@ public class UserServiceTest {
     @Autowired
     private UserService userService;
 
+    @MockBean
+    private UserEntityRepository userEntityRepository;
+
     @Test
     void SignUp_Success() {
         String userName = "userName";
         String password = "password";
 
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
+        when(userEntityRepository.save(any())).thenReturn(Optional.of(mock(UserEntity.class)));
+
         Assertions.assertDoesNotThrow(() -> userService.join(userName, password));
     }
+
+    @Test
+    void SignUp_Already_Exist_Name() {
+        String userName = "userName";
+        String password = "password";
+
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(mock(UserEntity.class));
+        when(userEntityRepository.save(any())).thenReturn(Optional.of(mock(UserEntity.class)));
+
+        Assertions.assertThrows(SnsApplicationException.class, () -> userService.join(userName, password));
+    } 
 }
